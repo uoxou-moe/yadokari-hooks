c1c7324599c326fa01a26c2de24293693c7b2311
Zemelua <zemelua1107@gmail.com>
2025-08-18 05:28:40 +0900

    こんにちは

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.model.PluginDependency
+
+plugins {
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "2.2.0"
+}
+
+group = "moe.uoxou"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.spongepowered.org/maven/") {
+        name = "spongepowered-repo"
+    }
+}
+
+sponge {
+    apiVersion("13.0.0")
+    license("All-Rights-Reserved")
+    loader {
+        name(PluginLoaders.JAVA_PLAIN)
+        version("1.0")
+    }
+    plugin("yadokari_hooks") {
+        displayName("Yadokari Hooks 🌠")
+        entrypoint("moe.uoxou.yadokari_hooks.YadokariHooks")
+        description("🌠")
+        links {
+            // homepage("https://spongepowered.org")
+            // source("https://spongepowered.org/source")
+            // issues("https://spongepowered.org/issues")
+        }
+        contributor("uoxou") {
+            description("Author")
+        }
+        dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+    }
+}
+
+val javaTarget = 21 // Sponge targets a minimum of Java 21
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(javaTarget))
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
